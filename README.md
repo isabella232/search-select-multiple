368bcec355cbe9adc20c59bb5668318a57eaf390
mofya1 <mofya@surveycto.com>
2020-05-07 10:41:16 +0200

    update ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# search-select-multiple
-Allow searching on long choice lists
+# Search-select-multiple
+
+![Default appearance for the 'search-select-multiple' field plug-in](extras/default-search.jpg)
+
+## Description
+
+This builds on the [baseline-select_multiple fieldplugin](https://github.com/surveycto/baseline-select_multiple), by adding the ability to ***filter*** the options by searching when using the default
+
+[![Download now](extras/download-button.png)](https://github.com/surveycto/search-select-multiple/raw/master/search-select-multiple.fieldplugin.zip)
+
+## Default SurveyCTO feature support
+
+| Feature / Property | Support |
+| --- | --- |
+| Supported field type(s) | `select_multiple`|
+| Default values | Yes |
+| Custom constraint message | Yes |
+| Custom required message | Yes |
+| Read only | Yes |
+| media:image | Yes |
+| media:audio | Yes |
+| media:video | Yes |
+| `minimal` appearance | No |
+| `compact` appearance | No |
+| `compact-#` appearance | No |
+
+## Expanded feature support
+
+In addition to supporting the default SurveyCTO features listed above, this field plug-in offers the following expanded functionality:
+
+1. Support for RTL languages  
+    If your *label*, *hint*, or choice labels are in a language that uses a right-to-left alphabet (like Arabic), they will be right-justified, and the checkboxes will be to the right of the choice labels. If your form contains both right-to-left and left-to-right choice labels, the formatting will respond to the currently-selected form language.
+1. Support for HTML in choice labels  
+    This plug-in will allow HTML in choice labels to be rendered as HTML, even in Collect on Android and iOS.
+1. Support for HTML in field references  
+    If you reference another field's value in either the field *label* or field *hint*, and that referenced value contains HTML, the HTML will be correctly rendered.
+
+## How to use
+
+**To use this plug-in as-is**, just download the [search-select-multiple.fieldplugin.zip](https://github.com/surveycto/search-select-multiple/raw/master/search-select-multiple.fieldplugin.zip) file from this repo, and attach it to your form.
+
+To create your own field plug-in using this as a template, follow these steps:
+
+1. Fork this repo
+1. Make changes to the files in the `source` directory.
+
+    * **Note:** be sure to update the `manifest.json` file as well.
+
+1. Zip the updated contents of the `source` directory.
+1. Rename the .zip file to *yourpluginname*.fieldplugin.zip (replace *yourpluginname* with the name you want to use for your plug-in).
+1. You may then attach your new .fieldplugin.zip file to your form as normal.
+
+## More resources
+
+* **Test form**  
+You can find a sample form definition here.
+[Download sample form](https://github.com/surveycto/search-select-multiple/raw/master/extras/sample-form/plugin_test_form_search_select_multiple.xlsx)
+
+* **Developer documentation**  
+Instructions and resources for developing your own field plug-ins.  
+[https://github.com/surveycto/Field-plug-in-resources](https://github.com/surveycto/Field-plug-in-resources)
+
+* **User documentation**  
+How to get started using field plug-ins in your SurveyCTO form.  
+[https://docs.surveycto.com/02-designing-forms/03-advanced-topics/06.using-field-plug-ins.html](https://docs.surveycto.com/02-designing-forms/03-advanced-topics/06.using-field-plug-ins.html)
--- source/script.js
+++ source/script.js
@@ -1,71 +1,69 @@
 // References to the elements in the field
-var checkBoxesContainer = document.getElementById("checkboxes-container");
-var choicesEl = document.getElementsByClassName("choice-container");
-var buttons = document.querySelectorAll('input[name="opt"]');
-var input = document.getElementById('filter-text');
+var checkBoxesContainer = document.getElementById('checkboxes-container')
+var choicesEl = document.getElementsByClassName('choice-container')
+var buttons = document.querySelectorAll('input[name="opt"]')
+var input = document.getElementById('filter-text')
 
 // Detect right-to-left languages
-function isRTL(s){
-  var ltrChars    = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
-      rtlChars    = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
-      rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+function isRTL (s) {
+  var ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF' + '\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF'
+  var rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC'
+  var rtlDirCheck = new RegExp('^[^' + ltrChars + ']*[' + rtlChars + ']')
 
-  return rtlDirCheck.test(s);
+  return rtlDirCheck.test(s)
 }
 
 if (fieldProperties.LANGUAGE !== null && isRTL(fieldProperties.LANGUAGE)) {
-  checkBoxesContainer.dir = "rtl";
+  checkBoxesContainer.dir = 'rtl'
 }
 // Add filtering of response options
-input.addEventListener('keyup', function(e) {
-  var query = $.trim($('#filter-text').val()).toLowerCase();
-  $('div.radio .search').each(function(){
-         var $this = $(this);
-         if($this.text().toLowerCase().indexOf(query) === -1)
-             $this.closest('div.radio').fadeOut();
-        else $this.closest('div.radio').fadeIn();
-    });
-});
+input.addEventListener('keyup', function (e) {
+  var query = $.trim($('#filter-text').val()).toLowerCase()
+  $('div.radio .search').each(function () {
+    var $this = $(this)
+    if ($this.text().toLowerCase().indexOf(query) === -1) { $this.closest('div.radio').fadeOut() } else $this.closest('div.radio').fadeIn()
+  })
+})
 
 // Define what happens when the user attempts to clear the response
 
-function clearAnswer() {
+function clearAnswer () {
   // go through each choice
   for (var i = 0; i < choicesEl.length; i++) {
-    choicesEl[i].querySelector('input').checked = false; // uncheck the checkbox input element
-    fieldProperties.CHOICES[i].CHOICE_SELECTED = false; // update the current answer 
+    choicesEl[i].querySelector('input').checked = false // uncheck the checkbox input element
+    fieldProperties.CHOICES[i].CHOICE_SELECTED = false // update the current answer
   }
 }
 
 // Save the user's response (update the current answer)
 
-function change() {
-  var selected = [];
+function change () {
+  var selected = []
   // go through each choice
   for (var i = 0; i < fieldProperties.CHOICES.length; i++) {
-    var choice = fieldProperties.CHOICES[i];
+    var choice = fieldProperties.CHOICES[i]
     if (choice.CHOICE_VALUE === this.value) { // check/uncheck based on the value of the choice
-      choice.CHOICE_SELECTED = !choice.CHOICE_SELECTED;
+      choice.CHOICE_SELECTED = !choice.CHOICE_SELECTED
     }
     if (choice.CHOICE_SELECTED) { // include only selected options in the final answer
-      selected.push(choice.CHOICE_VALUE);
+      selected.push(choice.CHOICE_VALUE)
     }
   }
 
-  setAnswer(selected.join(" "));
+  setAnswer(selected.join(' '))
 }
 
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].onchange = change;
+  buttons[i].onchange = change
 }
 
 // If the field label or hint contain any HTML that isn't in the form definition, then the < and > characters will have been replaced by their HTML character entities, and the HTML won't render. We need to turn those HTML entities back to actual < and > characters so that the HTML renders properly. This will allow you to render HTML from field references in your field label or hint.
-function unEntity(str){
-  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+function unEntity (str) {
+  return str.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
 }
 if (fieldProperties.LABEL) {
-  document.querySelector(".label").innerHTML = unEntity(fieldProperties.LABEL);
+  document.querySelector('.label').innerHTML = unEntity(fieldProperties.LABEL)
 }
 if (fieldProperties.HINT) {
-  document.querySelector(".hint").innerHTML = unEntity(fieldProperties.HINT);
+  document.querySelector('.hint').innerHTML = unEntity(fieldProperties.HINT)
 }
